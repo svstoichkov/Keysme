adadc3a2a81ce0c57d7c83bdb2a52ef9d086adee
svstoichkov <sv.stoichkov@yahoo.com>
2016-04-02 22:11:46 +0300

    Implemented repository pattern

---
+++ Source/Data/Keysme.Data/IKeysmeDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Keysme.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public interface IKeysmeDbContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
---
+++ Source/Keysme.Web/App_Start/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Keysme.Web.App_Start
+{
+    using System.Data.Entity;
+
+    using Data;
+    using Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<KeysmeDbContext, Configuration>());
+            KeysmeDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
