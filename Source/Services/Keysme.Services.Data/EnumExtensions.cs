42f804a578eb1338f1eb597832f59c9c7eef37b6
Svetlozar Stoichkov <sv.stoichkov@yahoo.com>
2016-04-16 23:45:23 +0300

    Host details page is ready

---
+++ Source/Services/Keysme.Services.Data/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Keysme.Services.Data
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var member = enumValue.GetType().GetMember(enumValue.ToString()).First().GetCustomAttribute<DisplayAttribute>();
+            if (member != null)
+            {
+                return member.GetName();
+            }
+
+            return enumValue.ToString();
+        }
+    }
+}
